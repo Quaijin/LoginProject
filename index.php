cf261a40757650757b6cb37f2143e11a9f5b04f5
Luis Baldera <luisbaldera21@gmail.com>
2016-11-18 22:52:45 -0400

    Testing NetBeans with Git

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>This is a Login Test Website</title>
+        <link type="text/css" rel="stylesheet" href="css/_style.css">
+    </head>
+    <body>
+        <article>
+            <h1> This is the Header</h1>
+            <p>this is a paragrah</p>
+        </article>
+        <?php
+        echo 'this is a php line';
+        ?>
+    </body>
+</html>
